baec8b1ac143fbe86204be43dcaae2832afa6fa5
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-30 23:13:07 +0900

    Change authentication function return

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,12 @@
 import crypto from 'crypto';
 
-const SECRET = 'YOUTUBE-REST=API';
+const SECRET = 'YOUTUBE-REST-API';
+
+export const authentication = (salt: string, password: string): string => {
+  return crypto
+    .createHmac('sha256', [salt, password].join('/'))
+    .update(SECRET)
+    .digest('hex');
+};
 
 export const random = () => crypto.randomBytes(128).toString('base64');
-export const authentication = (salt: string, password: string) =>
-  crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
