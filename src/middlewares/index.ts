8be2f4999659a8d01849b34bf819c6fc63b4c30e
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-31 02:51:03 +0900

    Add

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { merge } from 'lodash';
+import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users';
 
@@ -20,6 +20,27 @@ export const isAuthenticated = async (
     }
 
     merge(req, { identity: existingUser });
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, 'identity._id') as string;
+    if (!currentUserId) {
+      return res.sendStatus(400);
+    }
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(400);
+    }
     return next();
   } catch (error) {
     console.log(error);
