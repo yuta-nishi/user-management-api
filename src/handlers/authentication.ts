b0d2d660ec64b2576739415956fb0ab1c89c9906
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-30 22:18:46 +0900

    Change import path for my module

--- src/handlers/authentication.ts
+++ src/handlers/authentication.ts
@@ -1,6 +1,7 @@
-import { createUser, getUserByEmail } from 'db/users';
 import express from 'express';
-import { authentication, random } from 'helpers';
+
+import { createUser, getUserByEmail } from '../db/users';
+import { authentication, random } from '../helpers';
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
