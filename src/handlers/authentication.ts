dfe98e9df763d25a34ec91e590af2f1806649fae
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-30 23:52:02 +0900

    Add authentication login

--- src/handlers/authentication.ts
+++ src/handlers/authentication.ts
@@ -3,6 +3,39 @@ import express from 'express';
 import { createUser, getUserByEmail } from '../db/users';
 import { authentication, random } from '../helpers';
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt + authentication.password'
+    );
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+    if (user.authentication.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+    await user.save();
+    res.cookie('YOUTUBE-AUTH', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: '/',
+    });
+    return res.status(200).json(user).end();
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
@@ -12,13 +45,13 @@ export const register = async (req: express.Request, res: express.Response) => {
 
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
-      res.sendStatus(400);
+      return res.sendStatus(400);
     }
 
     const salt = random();
     const user = await createUser({
-      username,
       email,
+      username,
       authentication: {
         salt,
         password: authentication(salt, password),
