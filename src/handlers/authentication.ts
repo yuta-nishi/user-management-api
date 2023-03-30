665b642c35f685ed049c62c288f0336cb756be7f
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-31 02:55:41 +0900

    Fix typo

--- src/handlers/authentication.ts
+++ src/handlers/authentication.ts
@@ -59,7 +59,7 @@ export const register = async (req: express.Request, res: express.Response) => {
       },
     });
 
-    return res.sendStatus(200).json(user).end();
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
