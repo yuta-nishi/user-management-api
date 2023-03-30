19ddf96ab789d245b7eebbd4fcc63532b236aa60
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-31 02:52:17 +0900

    Add user data manipulation

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
-import { getAllUsers } from '../handlers/users';
-import { isAuthenticated } from '../middlewares';
+import { deleteUser, getAllUsers, updateUser } from '../handlers/users';
+import { isAuthenticated, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
   router.get('/users', isAuthenticated, getAllUsers);
+  router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
+  router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
 };
