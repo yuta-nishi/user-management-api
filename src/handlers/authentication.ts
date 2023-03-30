19a5d6b362e20779072d7625b643594012e89d09
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-30 23:56:14 +0900

    Update login error handling

--- src/handlers/authentication.ts
+++ src/handlers/authentication.ts
@@ -32,6 +32,7 @@ export const login = async (req: express.Request, res: express.Response) => {
     });
     return res.status(200).json(user).end();
   } catch (error) {
+    console.log(error);
     return res.sendStatus(400);
   }
 };
