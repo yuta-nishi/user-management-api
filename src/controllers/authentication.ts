cbffb648bb89997fda2bbc1a2dc3f63a322b493c
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-30 19:53:36 +0900

    Add authentication controller

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,32 @@
+import { createUser, getUserByEmail } from 'db/users';
+import express from 'express';
+import { authentication, random } from 'helpers';
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      username,
+      email,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.sendStatus(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
