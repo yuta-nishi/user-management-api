a3a278a9a2631d257b4542e87aa2693d6005d0ca
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-31 00:09:30 +0900

    Change UserSchema's property

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
   email: { type: String, required: true },
+  username: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
     salt: { type: String, select: false },
@@ -14,11 +14,11 @@ export const UserModel = mongoose.model('User', UserSchema);
 
 export const getUsers = () => UserModel.find();
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
-export const getUserBySessionToken = (sessionTaken: string) =>
-  UserModel.findOne({ 'authentication.sessionToken': sessionTaken });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({ 'authentication.sessionToken': sessionToken });
 export const getUserById = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) =>
   new UserModel(values).save().then((user) => user.toObject());
-export const deleteUserById = (id: string) => UserModel.findByIdAndDelete({ _id: id });
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
 export const updateUserById = (id: string, values: Record<string, any>) =>
   UserModel.findByIdAndUpdate(id, values);
